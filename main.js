2c7791757787add4981c84108d34d6a6d127ece4
gi511 <sar84255@gmail.com>
2024-10-16 10:04:08 +0300

    iphone

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let contaier = document.querySelector('.container');
+
+function phones(phone){
+    img.src = phone;
+}
+
+function colors(color){
+    contaier.style.background = color;
+}
